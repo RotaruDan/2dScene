1962391e92776dccfecacad1b7ffef6f30383c32
RotaruDan <drotaru@ucm.es>
2015-05-13 19:33:18 +0200

    Adds Actor and Group.

---
+++ freeglut project 2D MS Visual 2012/Actor.cpp
@@ -0,0 +1,52 @@
+//---------------------------------------------------------------------------
+// Dan Cristian, Rotaru
+//---------------------------------------------------------------------------
+#include "Actor.h"
+
+	Actor::Actor(){
+		r = 0.0;
+		g = 0.0;
+		b = 0.0; 
+		a = 1.0;
+		x = 0.0;
+		y = 0.0;
+	}
+
+	Actor::~Actor(){
+
+	}
+
+	GLdouble Actor::getX(){
+		return x;
+	}
+
+	GLdouble Actor::getY(){
+		return y;
+	}
+
+	void Actor::setColor(GLfloat r, GLfloat g, GLfloat b, GLfloat a){
+		Actor::r = r;
+		Actor::g = g;
+		Actor::b = b;
+		Actor::a = a;
+	}
+
+	void Actor::setX(GLdouble x){
+		Actor::x = x;
+	}
+
+	void Actor::setY(GLdouble y){
+		Actor::y = y;
+	}
+
+	void Actor::render(){
+		glPushMatrix();
+		glColor4f(r, g, b, a);
+		glTranslated(x, y, 0);
+		draw();
+		glPopMatrix();
+	}
+
+	void Actor::draw(){
+
+	}
---
+++ freeglut project 2D MS Visual 2012/Actor.h
@@ -0,0 +1,40 @@
+//---------------------------------------------------------------------------
+// Dan Cristian, Rotaru
+//---------------------------------------------------------------------------
+#ifndef ActorH
+#define ActorH
+
+#include <Windows.h>
+#include <gl/GL.h>
+#include <gl/GLU.h>
+
+class Actor{
+
+	private:
+		GLdouble x, y;
+
+	protected:
+		GLfloat r, g, b, a;
+
+	public:
+		Actor();
+		~Actor();
+
+		GLint getAngle();
+		GLdouble getAngleX();
+		GLdouble getAngleY();
+
+		GLdouble getX();
+		GLdouble getY();
+		
+		void setColor(GLfloat r, GLfloat g, GLfloat b, GLfloat a);
+
+		void setX(GLdouble x);
+		void setY(GLdouble y);
+
+		void render();
+
+		virtual void draw();
+};
+
+#endif
