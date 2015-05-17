0931dcef7e5d27ab6ca24dcc6ca60b55c45175a9
RotaruDan <drotaru@ucm.es>
2015-05-18 00:36:55 +0200

    Implemented some Triangle methods

--- freeglut project 2D MS Visual 2012/Triangle.cpp
+++ freeglut project 2D MS Visual 2012/Triangle.cpp
@@ -4,7 +4,6 @@
 #include "Triangle.h"
 
 	Triangle::Triangle() {
-
 	}
 
 	Triangle::~Triangle(){
@@ -12,11 +11,35 @@
 	}
 
 	void Triangle::draw(){		
-	  glBegin(GL_LINE_LOOP);
-	  for(int i = 0; i < vertex.size; ++i) {
-		  glVertex2d(vertex[i].getX(), vertex[i].getY());
-	  }
-	  glEnd();
+		if(mode == DESIGN) {
+			glBegin(GL_LINE_LOOP);
+			for(unsigned int i = 0; i < vertex.size(); ++i) {
+				glVertex2d(vertex[i].getX(), vertex[i].getY());
+			}
+			glEnd();
+		} else if (mode == SELECT) {
+			glBindTexture(GL_TEXTURE_2D, textureID);
+			glBegin(GL_TRIANGLES);
+			for(unsigned int i = 0; i < vertex.size(); ++i) {
+				glVertex2d(vertex[i].getX(), vertex[i].getY());
+				glTexCoord2f(texCoords[i].getX(), texCoords[i].getY());
+			}
+			glEnd();
+			if(selected) {
+				glBegin(GL_LINE_LOOP);
+				for(unsigned int i = 0; i < vertex.size(); ++i) {
+					glVertex2d(vertex[i].getX(), vertex[i].getY());
+				}
+				glEnd();
+			}
+		} else if (mode == ANIMATE) {
+			glBegin(GL_TRIANGLES);
+			for(unsigned int i = 0; i < vertex.size(); ++i) {
+				glVertex2d(vertex[i].getX(), vertex[i].getY());
+				glTexCoord2f(texCoords[i].getX(), texCoords[i].getY());
+			}
+			glEnd();
+		}
 	}
 
 	
@@ -28,8 +51,42 @@
 		Triangle::texCoords = texCoords;
 	}
 
+	void Triangle::computeTexCoords(GLfloat width, GLfloat height) {
+		PV2D vertex1 = vertex[0];
+		PV2D texCoord1(vertex1.getX()/width, vertex1.getY() / height);
+		PV2D vertex2 = vertex[1];
+		PV2D texCoord2(vertex2.getX()/width, vertex2.getY() / height);
+		PV2D vertex3 = vertex[2];
+		PV2D texCoord3(vertex3.getX()/width, vertex3.getY() / height);
+
+		texCoords.clear();
+		texCoords.push_back(texCoord1);
+		texCoords.push_back(texCoord2);
+		texCoords.push_back(texCoord3);
+	}
+	
+	void Triangle::computeBarycenter() {
+		GLfloat third = 0.333;
+		setBarycenter(vertex[0].getX() * third + vertex[1].getX() + vertex[2].getX(),
+			vertex[0].getY() * third + vertex[1].getY() + vertex[2].getY());
+	}
+	
 	bool Triangle::hit(GLfloat x, GLfloat y) {
 
+		for(unsigned int i = 0, n = vertex.size(); i < n; ++i) {
+			PV2D a = vertex[i];
+			PV2D b = vertex[(i+1) % n];
+			PV2D affineSegment(b.getX() - a.getX(), b.getY() - a.getY());
+			PV2D affinePoint(x - a.getX(), y - a.getY());
+
+			GLfloat xProduct = affineSegment.getX() * affinePoint.getY() - affineSegment.getY() * affinePoint.getX();
+
+			if(xProduct <= 0) {
+				return false;
+			}
+		}
+
+		return true;
 	}
 
 	void Triangle::setSelected(bool selected) {
@@ -37,7 +94,10 @@
 	}
 
 	void Triangle::move() {
-
+		barycenter.trasladar(&direction);
+		for(unsigned int i = 0; i < vertex.size(); ++i) {
+			vertex[i].trasladar(&direction);
+		}
 	}
 
 	void Triangle::setDirection(GLfloat x, GLfloat y) {
@@ -45,8 +105,23 @@
 		direction.setY(y);
 	}
 
+	void Triangle::firstVertex(GLfloat x, GLfloat y) {
+		PV2D point(x - vertex[0].getX(), y - vertex[0].getY());
+		for(unsigned int i = 0; i < vertex.size(); ++i) {
+			vertex[i].trasladar(&point);
+		}
+	}
 
 	void Triangle::setBarycenter(GLfloat x, GLfloat y) {
 		barycenter.setX(x);
 		barycenter.setY(y);
+	}
+
+	
+	void Triangle::setMode(Mode mode) {
+		Triangle::mode = mode;
+	}
+
+	void Triangle::setTextureId(GLuint textureID) {
+		Triangle::textureID = textureID;
 	}
--- freeglut project 2D MS Visual 2012/Triangle.h
+++ freeglut project 2D MS Visual 2012/Triangle.h
@@ -1,8 +1,8 @@
 //---------------------------------------------------------------------------
 // Dan Cristian, Rotaru
 //---------------------------------------------------------------------------
-#ifndef SphereH
-#define SphereH
+#ifndef TriangleH
+#define TriangleH
 
 #include "Actor.h"
 #include "PV2D.h"
@@ -10,13 +10,21 @@
 
 using namespace std;
 
+enum Mode { 
+	DESIGN, SELECT, ANIMATE
+};
+
 class Triangle : public Actor {
 
+	
+
 	private:
+		GLuint textureID;
 		vector<PV2D> vertex;
 		vector<PV2D> texCoords;
 		PV2D barycenter, direction;
 		bool selected;
+		Mode mode;
 
 	public:
 		Triangle();
@@ -24,11 +32,16 @@ class Triangle : public Actor {
 		void draw();
 		void setVertex(vector<PV2D> vertex);
 		void setTexCoords(vector<PV2D> texCoords);
+		void computeTexCoords(GLfloat width, GLfloat height);
 		bool hit(GLfloat x, GLfloat y);
 		void setSelected(bool selected);
 		void move();
 		void setDirection(GLfloat x, GLfloat y);
 		void setBarycenter(GLfloat x, GLfloat y);
+		void computeBarycenter();
+		void firstVertex(GLfloat x, GLfloat y);
+		void setMode(Mode mode);
+		void setTextureId(GLuint textureID);
 };
 
 #endif
